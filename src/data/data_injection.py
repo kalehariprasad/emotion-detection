c32e5aea9bd6fe81d3dd40ee1e67a75f5fca3369
Hari prasad <hariprasad9693@gmail.com>
2024-10-03 15:42:52 +0530

    modifications in utilities functions

--- src/data/data_injection.py
+++ src/data/data_injection.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from sklearn.model_selection import train_test_split
 from src.custom_logging import logging
 from src.exeption import CustomException
@@ -20,7 +21,12 @@ def main():
         train_data, test_data = train_test_split(
             final_df, test_size=test_size, random_state=42
         )
-        data_handler.save_data(train_data, test_data, data_path='./data')
+        data_path = os.path.join(os.getcwd(), "data")
+        raw_data_path = os.path.join(data_path, "raw")  # Corrected this line
+        train_file_path = os.path.join(raw_data_path, "train.csv")
+        test_file_path = os.path.join(raw_data_path, "test.csv")
+        data_handler.save_data(train_data, train_file_path)
+        data_handler.save_data(test_data, test_file_path)
     except Exception as e:
         logging.info('Failed to complete the data ingestion process: %s', e)
         raise CustomException(e, sys)
--- src/data/data_preprocessing.py
+++ src/data/data_preprocessing.py
@@ -6,9 +6,11 @@ import pandas as pd
 from src.custom_logging import logging
 from src.exeption import CustomException
 from src.utils import TextNormalizer
+from src.utils import DataHandler
 
 
 text_processor = TextNormalizer()  # Corrected variable name for consistency
+data_handler = DataHandler(params_path='params.yaml')
 
 
 def main():
@@ -21,18 +23,17 @@ def main():
         # Transform the data
         train_processed_data = text_processor.normalize_text(train_data)
         test_processed_data = text_processor.normalize_text(test_data)
-
         # Store the data inside data/interim
         data_path = os.path.join("./data", "interim")
         os.makedirs(data_path, exist_ok=True)
-
-        train_processed_data.to_csv(
-            os.path.join(data_path, "train_processed.csv"), index=False
-        )
-        test_processed_data.to_csv(
-            os.path.join(data_path, "test_processed.csv"), index=False
-        )
-        logging.info('Processed data  saved to %s', data_path)
+        train_processed_data_path = os.path.join(
+            data_path, "train_processed.csv"
+            )
+        test_processed_data_path = os.path.join(
+            data_path, "test_processed.csv"
+            )
+        data_handler.save_data(train_processed_data, train_processed_data_path)
+        data_handler.save_data(test_processed_data, test_processed_data_path)
     except Exception as e:
         logging.info('Failed to complete the data transformation : %s', e)
         raise CustomException(e, sys)
