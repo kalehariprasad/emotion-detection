f1fae7d1ab1e07a8b6bef76f2ba7e2302932cbdc
Hari prasad <hariprasad9693@gmail.com>
2024-10-08 18:23:44 +0530

    experiment name changed in model_evaluation file

--- src/models/model_evaluation.py
+++ src/models/model_evaluation.py
@@ -17,7 +17,7 @@ model = Model()
 
 
 def main():
-    mlflow.set_experiment("dvc-pipeline")
+    mlflow.set_experiment("Github-actions-dvc-pipeline")
     with mlflow.start_run() as run:  # Start an MLflow run
         try:
             clf = model.load_model('./models/model/model.pkl')
